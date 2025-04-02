26577af1d45ef1239bdc06f9a78aa45099f40fd3
Grigory_Artazyan <grigory@uni.minerva.edu>
2025-04-02 16:50:46 +0200

    new

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-from imports import *  # Import all necessary libraries and functions
+import matplotlib
+matplotlib.use('Agg')  # Ensures Matplotlib uses a backend compatible with Streamlit
+
+import streamlit as st
+import tempfile
+import cv2
+import numpy as np
+import mediapipe as mp
+import matplotlib.pyplot as plt
+import os
+import sys
+from streamlit_vertical_slider import vertical_slider
+from streamlit_option_menu import option_menu
+
+import menu_bar
 
 # Initialize Mediapipe for pose detectiongit status
 mp_pose = mp.solutions.pose  # Pose model for detecting body landmarks
